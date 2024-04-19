adacbf6b5d3da188c5c07abd418eec07b81fe8b1
João <jps.pereira0805@gmail.com>
2024-04-19 19:20:39 -0300

    grid layout em 'conhecimentos

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,25 @@ ScrollReveal().reveal('.img-content', {
 
 ScrollReveal().reveal('.text-content', { 
     rotate: { x: 0, y: 20, z:0},
-    delay: 300
+    delay: 200
 });
+
+ScrollReveal().reveal('.title', {
+    rotate: { x: 0, y: 20, z:0},
+    delay: 100
+})
+
+ScrollReveal().reveal('.grid-container', {
+    rotate: { x: 0, y: 20, z:0},
+    delay: 200
+}) 
 
 //JQUERY
 $(document).ready(function() {
+
+    //SOBREMIM
     function showContentAndSelectButton(btnId, contentId) {
-        $('.content').hide();
+        $('.sobremim-content').hide();
         $(contentId).show();
         $('.selected').removeClass('selected');
         $(btnId).addClass('selected');      
@@ -29,4 +41,46 @@ $(document).ready(function() {
     })
 
     showContentAndSelectButton('#mystory', '#mystory-content');
+
+    
+    //CONHECIMENTOS
+    $('.technology-description').hide();
+    $('.base-box').mouseenter(function() {
+        const elementId = $(this).attr('id');
+        $('.technology-description').stop(true, true).fadeOut();
+
+        let targetElement = null 
+        switch (elementId) {
+            case 'html':
+                targetElement = '.html-knowledge'
+                break
+            case 'css':
+                targetElement = '.css-knowledge'
+                break
+            case 'javascript':
+                targetElement = '.javascript-knowledge'
+                break
+            case 'nodejs':
+                targetElement = '.nodejs-knowledge'
+                break
+            case 'mysql':
+                targetElement = '.mysql-knowledge'
+                break
+            case 'mongodb':
+                targetElement = '.mongodb-knowledge'
+                break
+            case 'git':
+                targetElement = '.git-knowledge'
+                break
+            case 'github':
+                targetElement = '.github-knowledge'
+        }
+
+        if (targetElement) $(targetElement).fadeIn();
+    })
+
+    $('.base-box').mouseleave(function() {
+        $('.technology-description').stop(true, true).fadeOut()
+    })
+
 });
