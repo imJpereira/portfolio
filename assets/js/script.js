8a02c5d2d950a50bc37e7fdfa57b9ebe800d703d
João <jps.pereira0805@gmail.com>
2024-04-26 18:10:30 -0300

    página de projetos pronta

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+document.getElementById('password-link').addEventListener( 'click', () => {
+    const url = 'https://passmastermind.netlify.app/'
+    window.open(url)
+});
+
+document.getElementById('password-github').addEventListener( 'click', () => {
+    const url = 'https://github.com/imJpereira/password-generator?tab=readme-ov-file'
+    window.open(url)
+});
+
+document.getElementById('form-link').addEventListener( 'click', () => {
+    const url = 'https://valida-formulario.netlify.app/'
+    window.open(url)
+});
+
+document.getElementById('form-github').addEventListener( 'click', () => {
+    const url = 'https://github.com/imJpereira/form-validation?tab=readme-ov-file'
+    window.open(url)
+});
+
+document.getElementById('cadastro-github').addEventListener( 'click', () => {
+    const url = 'https://github.com/imJpereira/cadastro'
+    window.open(url)
+});
