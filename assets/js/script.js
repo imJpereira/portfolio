128b720e8b352e82b1353299a5dfa48d8294ba40
João <jps.pereira0805@gmail.com>
2024-04-28 12:44:30 -0300

    portifolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,33 @@
+//DOWNLOAD CV
+document.getElementById('cv-download').addEventListener('click', () => {
+    const fileUrl = '/assets/download/curriculo.pdf'
+
+    const linkDownload = document.createElement('a');
+    linkDownload.href = fileUrl;
+    linkDownload.download = 'curriculo.pdf';
+    document.body.appendChild(linkDownload);
+    
+    linkDownload.click();
+
+    document.body.removeChild(linkDownload);
+});
+
+
+// LINKTREE
+document.getElementById('contact').addEventListener('click', () => {
+    const url = 'https://linktr.ee/Joao_Pereira_'
+    window.open(url, '_self')
+});
+
+
+// PROJETOS
 document.getElementById('password-link').addEventListener( 'click', () => {
     const url = 'https://passmastermind.netlify.app/'
     window.open(url)
 });
 
 document.getElementById('password-github').addEventListener( 'click', () => {
-    const url = 'https://github.com/imJpereira/password-generator?tab=readme-ov-file'
+    const url = 'https://github.com/imJpereira/password-generator'
     window.open(url)
 });
 
@@ -14,7 +37,17 @@ document.getElementById('form-link').addEventListener( 'click', () => {
 });
 
 document.getElementById('form-github').addEventListener( 'click', () => {
-    const url = 'https://github.com/imJpereira/form-validation?tab=readme-ov-file'
+    const url = 'https://github.com/imJpereira/form-validation'
+    window.open(url)
+});
+
+document.getElementById('cpf-link').addEventListener( 'click', () => {
+    const url = 'https://cpf-gen.netlify.app/'
+    window.open(url)
+}); 
+
+document.getElementById('cpf-github').addEventListener( 'click', () => {
+    const url = 'https://github.com/imJpereira/gerador-de-cpf'
     window.open(url)
 });
 
